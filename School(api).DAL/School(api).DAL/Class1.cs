ac3d21829b1126cb520ff2ac20356b6d7aff49b1
simplyroot <49658084+simplyroot@users.noreply.github.com>
2022-06-09 03:13:51 +0300

    Add files via upload

---
+++ School(api).DAL/School(api).DAL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace School_api_.DAL
+{
+    public class Class1
+    {
+        //entity data model ekleyebilmek için (entityframwork değil ya) dependencies sağ tık nu package microsoftentityframeworkcore 5.0 indir
+        //tools, package manager console Scaffold-DbContext "Data Source=.;Initinal Catalog=School;Intagrated Security=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Entities
+        //entity framework core sql serve da indir
+        //hata vericek, hocada da öyle oldu
+    }
+}
